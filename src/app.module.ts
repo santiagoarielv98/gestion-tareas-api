b67b35dda7fb2ffd7e22ef7422a7ec6eedb2c4e6
Santiago Ariel <santiagoarielv98@gmail.com>
2023-09-22 12:13:10 -0300

    se genero nuevo recurso: tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DatabaseModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
