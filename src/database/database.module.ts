8f1649a120e0ed5c7093d0bcac3e9b1f686bf56d
Santiago Ariel <santiagoarielv98@gmail.com>
2023-09-22 11:28:58 -0300

    configuracion para conexion con base de datos mongodb

---
+++ src/database/database.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI ?? 'mongodb://localhost:27017/gestion-tareas'),
+  ],
+  exports: [MongooseModule],
+})
+export class DatabaseModule {}
