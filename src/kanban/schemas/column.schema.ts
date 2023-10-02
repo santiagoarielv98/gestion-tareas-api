984d8592eaed953f2b4541e0454c9867ebb58db0
Santiago Ariel <santiagoarielv98@gmail.com>
2023-10-02 14:55:25 -0300

    se actualizo el esquema de column

--- src/kanban/schemas/column.schema.ts
+++ src/kanban/schemas/column.schema.ts
@@ -1,18 +1,18 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { HydratedDocument } from 'mongoose';
+import mongoose, { Document, HydratedDocument } from 'mongoose';
 import { Task } from 'src/tasks/schemas/task.schema';
 
 export type ColumnDocument = HydratedDocument<Column>;
 
 @Schema({ versionKey: false })
-export class Column extends mongoose.Document {
+export class Column extends Document {
   @Prop({ required: true })
   title: string;
 
   @Prop({ default: '' })
   description: string;
 
-  @Prop({ index: true })
+  @Prop({ required: true })
   position: number;
 
   @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Task' }] })
